29c6810ab355f5850193e01901fca6e70312b9bc
Bertug <bertugyemen@outlook.com>
2020-05-07 17:10:30 +0300

    Add files via upload

--- AddO365GroupsToExpirationPolicy.ps1
+++ AddO365GroupsToExpirationPolicy.ps1
@@ -5,18 +5,21 @@ event, its authors, or anyone else involved in the creation, production, or deli
 loss) arising out of the use of or inability to use the sample scripts or documentation#>
 
 
-
+#Connect EXO
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session -DisableNameChecking
 
+#Connect Azure AD
 Connect-AzureAD
 
-
+#Assigning all groups with the custom attruibute of Expire
 Get-UnifiedGroup | Set-UnifiedGroup -CustomAttribute1 "Expire"
 
 [string[]]$Exceptions = @()
 
+#Requesting groups which are not going to be expired
+#Changing the customattribute1 to null
 do{
 $input = Read-Host " What is the name of the group that you would like to exclude?"
 $Exceptions += $input
@@ -29,16 +32,17 @@ ForEach($i in $Count){
     Set-UnifiedGroup  -Identity $Exceptions[$i] -CustomAttribute1 $null
 }
 
+
+#storing all groups with customattribute1 to a variable
 $ExpireGroups = Get-UnifiedGroup | Where{$_.customAttribute1 -eq "Expire"}
 
-
-
+#getting expiration policyID
 $PolicyId = (Get-AzureADMSGroupLifecyclePolicy).Id
+
+#Expiration Policy to Selected so not All or None.
 Set-AzureADMSGroupLifecyclePolicy -ID $policyID -ManagedGroupTypes "Selected"
 
-
-
-
+#Adding groups to the policy
 ForEach($group in $ExpireGroups){
     
     $CheckPolicy = (Get-UnifiedGroup -Identity $group.ExternalDirectoryObjectId).CustomAttribute2
@@ -48,6 +52,8 @@ ForEach($group in $ExpireGroups){
     ##Remove-AzureADMSLifecyclePolicyGroup -GroupId $group.ExternalDirectoryObjectId -Id $PolicyId -ErrorAction SilentlyContinue
     Add-AzureADMSLifecyclePolicyGroup -GroupId $group.ExternalDirectoryObjectId -Id $PolicyId -ErrorAction SilentlyContinue
     
+    
+    ##Setting customattribute2 to the expiration policy ID so it will be easy to identify in the future
     Set-UnifiedGroup -Identity $group.externalDirectoryobjectID -CustomAttribute2 $PolicyId
     
     $Counter++ 
